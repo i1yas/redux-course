c4c6096267bfeed84859b649cfeafd8205bc1464
i1yas <ilyasers@gmail.com>
2017-07-20 20:17:42 +0300

    Add User and Page components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import User from './../components/User'
+import Page from './../components/Page'
 
 class App extends Component {
   render() {
-    const { name, surname} = this.props.user
-    const { year, photos } = this.props.page
+    const { user, page } = this.props
     return <div>
-      <p>Привет из App, {name} {surname}!</p>
-      <p>У тебя уже {photos.length} фото за {year} год</p>
+      <User name={user.name} />
+      <Page photos={page.photos} year={page.year} />
     </div>
   }
 }
