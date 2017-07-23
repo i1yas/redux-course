529e56ef605a76f9150785def101edc504a0daf3
i1yas <ilyasers@gmail.com>
2017-07-23 16:11:28 +0300

    Add styles for Page component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './../styles/page.css'
 
 export default class Page extends Component {
   onYearBtnClick(e) {
-    this.props.setYear(+e.target.textContent)
+    this.props.setYear(+e.target.textContent);
   }
   render() {
-    const { year, photos } = this.props
-    return <div>
-      <p>
-        <button onClick={::this.onYearBtnClick}>2015</button>
-        <button onClick={::this.onYearBtnClick}>2016</button>
-        <button onClick={::this.onYearBtnClick}>2017</button>
-      </p>
-      <h3>{year} год</h3>
-      <p>У тебя {photos.length}.</p>
-    </div>
+    const { year, photos } = this.props;
+    return (
+      <div className='page'>
+        <p className='page__button-container'>
+          <button className='page__button' onClick={::this.onYearBtnClick}>
+            2015
+          </button>
+          <button className='page__button' onClick={::this.onYearBtnClick}>
+            2016
+          </button>
+          <button className='page__button' onClick={::this.onYearBtnClick}>
+            2017
+          </button>
+        </p>
+        <h3 className='page__title'>{year} год</h3>
+        <p className='page__count'>У тебя {photos.length} фоток.</p>
+      </div>
+    );
   }
 }
 
@@ -23,4 +32,4 @@ Page.propTypes = {
   year: PropTypes.number.isRequired,
   photos: PropTypes.array.isRequired,
   setYear: PropTypes.func.isRequired
-}
+};
